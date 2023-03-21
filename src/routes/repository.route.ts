880a00a1f100392eb1f04c621c03bcaf543814d7
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-21 00:33:35 -0500

    repository router updated: initial repository validator added

--- src/routes/repository.route.ts
+++ src/routes/repository.route.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 
 import { RepositoryController } from "../controllers";
+import { validateCreateRepository } from "../validators";
 
 const router = Router();
 
 const repositoryController = new RepositoryController();
 
-router.post("/", repositoryController.createRepository);
+router.post(
+  "/",
+  validateCreateRepository,
+  repositoryController.createRepository
+);
+
 router.get("/", repositoryController.getRepositories);
 
 export default router;
