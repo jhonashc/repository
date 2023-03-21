ad12d3d1f76924a0ed6c907a406e515aeab59970
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-20 22:38:49 -0500

    user router updated: common and user validator added

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,16 +1,29 @@
 import { Router } from "express";
 
 import { UserController } from "../controllers";
-import { validateCreateUser } from "../validators";
+import {
+  validateCreateUser,
+  validateUpdateUser,
+  validateUuidParam,
+} from "../validators";
 
 const router = Router();
 
 const userController = new UserController();
 
 router.post("/", validateCreateUser, userController.createUser);
+
 router.get("/", userController.getUsers);
-router.get("/:id", userController.getUserById);
-router.patch("/:id", userController.updateUserById);
-router.delete("/:id", userController.deleteUserById);
+
+router.get("/:id", validateUuidParam, userController.getUserById);
+
+router.patch(
+  "/:id",
+  validateUuidParam,
+  validateUpdateUser,
+  userController.updateUserById
+);
+
+router.delete("/:id", validateUuidParam, userController.deleteUserById);
 
 export default router;
