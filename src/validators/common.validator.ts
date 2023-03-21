c9d9701d33c444688d8c0909c921be9e15d35345
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-21 01:51:21 -0500

    common validator updated: validate pagination query added

--- src/validators/common.validator.ts
+++ src/validators/common.validator.ts
@@ -6,3 +6,9 @@ export const validateUuidParam = [
   check("id").isUUID().withMessage("the id must be an uuid"),
   ValidateRequest,
 ];
+
+export const validatePaginationQuery = [
+  check("limit").optional().isNumeric().withMessage("limit must be a number"),
+  check("offset").optional().isNumeric().withMessage("offset must be a number"),
+  ValidateRequest,
+];
--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,3 +1,3 @@
-export { validateUuidParam } from "./common.validator";
+export { validatePaginationQuery, validateUuidParam } from "./common.validator";
 export { validateCreateRepository } from "./repository.validator";
 export { validateCreateUser, validateUpdateUser } from "./user.validator";
