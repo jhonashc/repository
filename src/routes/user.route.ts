4d81924e843f52a6c7222c6acfda99bac0a94041
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-20 21:40:39 -0500

    user router updated: validate create user added

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
 import { UserController } from "../controllers";
+import { validateCreateUser } from "../validators";
 
 const router = Router();
 
 const userController = new UserController();
 
-router.post("/", userController.createUser);
+router.post("/", validateCreateUser, userController.createUser);
 router.get("/", userController.getUsers);
 router.get("/:id", userController.getUserById);
 router.patch("/:id", userController.updateUserById);
