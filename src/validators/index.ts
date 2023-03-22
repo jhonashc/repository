18f5fe4edf763162cb16ebd683226e82150d6a6b
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-22 01:37:14 -0500

    auth validator added: validate login and register added

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,3 +1,4 @@
+export { validateLoginUser, validateRegisterUser } from "./auth.validator";
 export { validatePaginationQuery, validateUuidParam } from "./common.validator";
 export { validateCreateRepository } from "./repository.validator";
 export { validateCreateUser, validateUpdateUser } from "./user.validator";
