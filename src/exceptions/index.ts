ac7e3e924463d94613be03365a5a92a8c41b85bb
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-22 01:33:44 -0500

    unauthorized exception added

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -2,3 +2,4 @@ export { BaseError } from "./base.exception";
 export { BadRequestException } from "./bad-request.exception";
 export { ConflictException } from "./conflict.exception";
 export { NotFoundException } from "./not-found.exception";
+export { UnauthorizedException } from "./unauthorized.exception";
---
+++ src/exceptions/unauthorized.exception.ts
@@ -0,0 +1,11 @@
+import { BaseError } from "./base.exception";
+
+export class UnauthorizedException extends BaseError {
+  statusCode = 401;
+  message: string;
+
+  constructor(message: string = "Unauthorized") {
+    super(message);
+    this.message = message;
+  }
+}
