4b740bb2bbb3b312834ae098292f58ca578a648a
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-22 01:37:53 -0500

    auth router updated: register and validators added

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { AuthController } from "../controllers";
+import { validateLoginUser, validateRegisterUser } from "../validators";
 
 const router = Router();
 
 const authController = new AuthController();
 
-router.post("/register", authController.register);
+router.post("/login", validateLoginUser, authController.login);
+
+router.post("/register", validateRegisterUser, authController.register);
 
 export default router;
